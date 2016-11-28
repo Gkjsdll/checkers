2dca97d5c2828b114a04077d3a320175a8a09bd9
Zack Winchell <zackwinchell@gmail.com>
2016-11-27 17:34:50 -0800

    Implement empty game board render

--- index.jsx
+++ index.jsx
@@ -17,6 +17,7 @@ const initBoard = () =>{
       })})
 };
 
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -28,15 +29,24 @@ class App extends React.Component {
   render() {
     return (<div>
         <h1>Welcome to Checkers!</h1>
+        <GameBoard gameState={this.state.gameState} />
       </div>);
   };
   
 }
 
-const GameBoard = () => {
-
-}
-
+const GameBoard = (props) => (
+  <div className="gameboard">
+    {_.flatten(props.gameState).map((square, index) => {
+      const row = Math.floor(index / 8);
+      const col = index % 8;
+      if ((row + col) % 2) {
+        return <div key={index} className="square black"></div>;
+      }
+      return <div key={index} className="square white"></div>;
+    })}
+  </div>
+);
 
 ReactDOM.render(
   <App />,
